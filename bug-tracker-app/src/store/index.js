02c3cf441d5cab88dee73c4ccdf8ddb7fdfd1459
Magesh Kuppan <tkmagesh77@gmail.com>
2020-07-15 14:19:54 +0530

    logger middleware implementation

--- bug-tracker-app/src/store/index.js
+++ bug-tracker-app/src/store/index.js
@@ -1,6 +1,20 @@
-import { createStore } from 'redux';
-import rootReducer from '../reducers'
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers';
 
-const appStore = createStore(rootReducer);
+const loggerMiddleware = store => next => action => {
+    console.group(action.type);
+    console.log(action);
+    console.group('before');
+    console.log(store.getState());
+    console.groupEnd();
+    next(action);
+    console.group('after');
+    console.log(store.getState());
+    console.groupEnd();
+    console.groupEnd();
+}
+    
+
+const appStore = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 export default appStore;
